de36f2ac074dac0d3fa138c7d93ff85411348a4d
sagarimadattaabi <sagarima.datta@ab-inbev.com>
2023-09-07 18:18:08 +0530

    Configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """ Add x and y and return resuts."""
     return x + y
 
-def subtract(x,y):
-    """SUbtract x from y and return result"""
+
+def subtract(x, y):
+    """Subtract x from y and return result"""
     return y - x
